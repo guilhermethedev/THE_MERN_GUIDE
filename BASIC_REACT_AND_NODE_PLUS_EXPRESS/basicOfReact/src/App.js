f4b03ed7454d059c8317be77e307185eb05e04f8
guilhermethedev <guilherme.borges@bairesdev.com>
2022-09-11 16:55:13 -0300

    basicOfReact/ created explaning the basic render

---
+++ BASIC_REACT_AND_NODE_PLUS_EXPRESS/basicOfReact/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return <React.Fragment>
+    <h1>A React App!</h1>
+    <div className='box-red'></div>
+    <div className='box-green'></div>
+  </React.Fragment>
+};
+
+export default App;
