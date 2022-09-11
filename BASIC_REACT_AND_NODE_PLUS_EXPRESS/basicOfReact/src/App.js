8dcf8b1972d09729c52e72a57a443dea4b6bf6a1
guilhermethedev <guilherme.borges@bairesdev.com>
2022-09-11 20:44:44 -0300

    Box and Titles components organized and App class rendering each itself

--- BASIC_REACT_AND_NODE_PLUS_EXPRESS/basicOfReact/src/App.js
+++ BASIC_REACT_AND_NODE_PLUS_EXPRESS/basicOfReact/src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import Title from './components/Title/Title';
+import Box from './components/Box/Box';
+import "./styles/App.css";
 
-const App = () => {
-  return <React.Fragment>
-    <h1>A React App!</h1>
-    <div className='box-red'></div>
-    <div className='box-green'></div>
-  </React.Fragment>
-};
+class App extends React.Component {
+  boxesOwners = ["Monka", "Iza", "Borges", "Raymund"]
+  renderingBoxes = () => {
+    return (
+      this.boxesOwners.map((owner, index) => {
+        if(index <= 1)
+          return <Box className={'box-red'} title={"IM A RED BOX..."} owner={owner} key={index}/>
+        return <Box className={'box-green'} title={"IM A GREEN BOX..."} owner={owner} key={index}/>
+      })
+    )
+  }
+  render() {
+    return <div className="container">
+      <Title nameOfCreator={this.props.nameOfCreator} />
+      {this.renderingBoxes()}
+    </div>
+  } 
+}
 
 export default App;
