6780172fd49081fbd8df6b234de45dc81ce8c071
guilhermethedev <guilherme.borges@bairesdev.com>
2022-09-13 18:41:49 -0300

    Map component currently working on PlaceItem

--- frontendSPA/src/places/components/PlaceItem.js
+++ frontendSPA/src/places/components/PlaceItem.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Button from "../../shared/components/FormElements/Button";
 import Card from "../../shared/components/UIElements/Card";
+import Map from "../../shared/components/UIElements/Map";
 import Modal from "../../shared/components/UIElements/Modal";
 import "./PlaceItem.css";
 
@@ -19,7 +20,11 @@ const PlaceItem = (props) => {
         footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
       >
         <div className="map-container">
-            <h2>THE MAP!</h2>
+            <Map 
+              center={props.coordinates}
+              zoom={16}
+              style={{width: 500, height: 250, position: "absolute", left: 75}}
+            />
         </div>
       </Modal>
       <li className="place-item">
