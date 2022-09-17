c411c7ecb06c48f470d639e112c1397d92504c4f
guilhermethedev <guilherme.borges@bairesdev.com>
2022-09-17 20:21:50 -0300

    The 2 GET routes for places id and places by users handling errors from models/error layer

--- backendForSPA/routes/places-routes.js
+++ backendForSPA/routes/places-routes.js
@@ -1,25 +1,40 @@
-const express = require('express');
+const express = require("express");
+const HttpError = require("../models/http-error");
 
 const router = express.Router();
 
 const DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Empire State Building',
-        description: 'One of the most famous sky scrapers in the world',
-        location: {
-            lat: 40.7484474,
-            lng: -73.9871516
-        },
-        address: "20 W 34th St, New York, NY 10001",
-        creator: "u1"
-    }
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world",
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516,
+    },
+    address: "20 W 34th St, New York, NY 10001",
+    creator: "u1",
+  },
 ];
 
-router.get('/:pid', (req, res, next) => {
-    const placeId = req.params.pid;
-    const place = DUMMY_PLACES.find(p => p.id === placeId)
-    res.json({ place });
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => p.id === placeId);
+  if (!place) {
+    throw new HttpError("Could not find a place for the provided id.", 404);
+  }
+  res.json({ place });
+});
+
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find((p) => p.creator === userId);
+  if (!place) {
+    return next(
+      new HttpError("Could not find a place for the provided user id.", 404)
+    );
+  }
+  res.json({ place });
 });
 
 module.exports = router;
