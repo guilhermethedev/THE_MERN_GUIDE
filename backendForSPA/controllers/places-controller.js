bb33cafcc930daf7c07735f42fe45f5dd7235a8e
guilhermethedev <guilherme.borges@bairesdev.com>
2022-09-18 20:15:28 -0300

    POST request created on place-controller

--- backendForSPA/controllers/places-controller.js
+++ backendForSPA/controllers/places-controller.js
@@ -1,3 +1,4 @@
+const {v4: uuid} = require("uuid");
 const HttpError = require("../models/http-error");
 
 const DUMMY_PLACES = [
@@ -34,5 +35,20 @@ const getPlaceByUserId = (req, res, next) => {
   res.json({ place });
 };
 
+const postPlace = (req, res, next) => {
+    const { title, description, coordinates, address, creator } = req.body;
+    const createdPlace = { 
+        id: uuid(),
+        title, 
+        description, 
+        location: coordinates, 
+        address, 
+        creator 
+    };
+    DUMMY_PLACES.push(createdPlace);
+    res.status(201).json({ place: createdPlace });
+};
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
+exports.postPlace = postPlace;
--- backendForSPA/routes/places-routes.js
+++ backendForSPA/routes/places-routes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get("/:pid", placesControllers.getPlaceById);
 router.get("/user/:uid", placesControllers.getPlaceByUserId);
+router.post("/", placesControllers.postPlace);
 
 module.exports = router;
