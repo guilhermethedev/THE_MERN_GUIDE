562a5637c74433c439d8e69e98e05d3a6bb32fe0
guilhermethedev <guilherme.borges@bairesdev.com>
2022-09-18 21:30:19 -0300

    places-controller has now a route to get all places per uid, not the first... deletion API working too as the PATCH route

--- backendForSPA/routes/places-routes.js
+++ backendForSPA/routes/places-routes.js
@@ -4,7 +4,9 @@ const placesControllers = require("../controllers/places-controller");
 const router = express.Router();
 
 router.get("/:pid", placesControllers.getPlaceById);
-router.get("/user/:uid", placesControllers.getPlaceByUserId);
+router.get("/user/:uid", placesControllers.getPlacesByUserId);
 router.post("/", placesControllers.postPlace);
+router.patch("/:pid", placesControllers.patchPlaceById);
+router.delete("/:pid", placesControllers.deletePlaceById);
 
 module.exports = router;
