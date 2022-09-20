e1e6e39f551244518a09fe18a444326f93a6a9d7
guilhermethedev <guilherme.borges@bairesdev.com>
2022-09-19 22:49:42 -0300

    All routes has a verification using express-validator

--- backendForSPA/controllers/places-controller.js
+++ backendForSPA/controllers/places-controller.js
@@ -1,4 +1,5 @@
 const {v4: uuid} = require("uuid");
+const { validationResult } = require("express-validator");
 const HttpError = require("../models/http-error");
 
 let DUMMY_PLACES = [
@@ -36,6 +37,10 @@ const getPlacesByUserId = (req, res, next) => {
 };
 
 const postPlace = (req, res, next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+      throw new HttpError('Invalid inputs passed, please check your data.', 422);
+    }
     const { title, description, coordinates, address, creator } = req.body;
     const createdPlace = { 
         id: uuid(),
@@ -50,6 +55,10 @@ const postPlace = (req, res, next) => {
 };
 
 const patchPlaceById = (req, res, next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+      throw new HttpError('Invalid inputs passed, please check your data.', 422);
+    }
     const { title, description } = req.body;
     const placeId = req.params.pid;
     const updatedPlace = {...DUMMY_PLACES.find(p => p.id === placeId)};
@@ -62,6 +71,9 @@ const patchPlaceById = (req, res, next) => {
 
 const deletePlaceById = (req, res, next) => {
     const placeId = req.params.pid;
+    if(!DUMMY_PLACES.find(p => p.id === placeId)) {
+      throw new HttpError("Could not find a place for that id.", 404)
+    }
     DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
     res.status(200).json({ message: "Place was deleted successfully." });
 }
--- backendForSPA/routes/places-routes.js
+++ backendForSPA/routes/places-routes.js
@@ -1,12 +1,28 @@
-const express = require("express");
+const { Router } = require("express");
+const { check } = require("express-validator");
 const placesControllers = require("../controllers/places-controller");
 
-const router = express.Router();
+const router = Router();
 
 router.get("/:pid", placesControllers.getPlaceById);
 router.get("/user/:uid", placesControllers.getPlacesByUserId);
-router.post("/", placesControllers.postPlace);
-router.patch("/:pid", placesControllers.patchPlaceById);
+router.post(
+    "/", 
+    [
+        check('title').not().isEmpty(),
+        check('description').isLength({ min: 5 }),
+        check('address').not().isEmpty()
+    ], 
+    placesControllers.postPlace
+);
+router.patch(
+    "/:pid", 
+    [
+        check('title').not().isEmpty(),
+        check('description').isLength({ min: 5 })
+    ], 
+    placesControllers.patchPlaceById
+);
 router.delete("/:pid", placesControllers.deletePlaceById);
 
 module.exports = router;
